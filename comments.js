e53fed7793acddbee4e3bdb9df5cf3bb10d52522
modirajendra <modirajendra4@gmail.com>
2024-12-25 23:50:37 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,18 @@
+// create web server
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var qs = require('querystring');
+
+// create server
+http.createServer(function(req, res) {
+    var pathname = url.parse(req.url).pathname;
+    if (pathname == '/') {
+        display_form(res);
+    } else if (pathname == '/comment') {
+        save_comment(req, res);
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('Not Found');
+    }
+}).listen(1337, '
