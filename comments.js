5861238188d83da689d395eed3b6cec70db250d6
modirajendra <modirajendra4@gmail.com>
2024-12-26 00:13:56 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,35 +1,55 @@
 // create web server
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
-const fs = require("fs");
-const bodyParser = require("body-parser");
+app.listen(3000);
 
-const DATA_FILE_PATH = path.join(__dirname, "data.json");
+// create comments array
+let comments = [
+    {username: 'Alice', body: 'I love cats!'},
+    {username: 'Bob', body: 'I love dogs!'},
+    {username: 'Charlie', body: 'I love parrots!'}
+];
 
-app.use(express.static("public"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/api/comments", (req, res) => {
-    fs.readFile(DATA_FILE_PATH, (err, data) => {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-        res.json(JSON.parse(data));
-    });
+// create GET route for /comments
+app.get('/comments', (req, res) => {
+    res.send(comments);
 });
 
-app.post("/api/comments", (req, res) => {
-    fs.readFile(DATA_FILE_PATH, (err, data) => {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-        const comments = JSON.parse(data);
-        const newComment = {
-            id: Date.now(),
-        };
-    });
+// create POST route for /comments
+app.post('/comments', (req, res) => {
+    let username = req.query.username;
+    let body = req.query.body;
+    comments.push({username: username, body: body});
+    res.send('Comment added');
 });
+
+// create DELETE route for /comments
+app.delete('/comments', (req, res) => {
+    let username = req.query.username;
+    comments = comments.filter(comment => {
+        return comment.username !== username;
+    });
+    res.send('Comment deleted');
+});
+
+// create PUT route for /comments
+app.put('/comments', (req, res) => {
+    let username = req.query.username;
+    let body = req.query.body;
+    comments.forEach(comment => {
+        if (comment.username === username) {
+            comment.body = body;
+        }
+    });
+    res.send('Comment updated');
+});
+
+// test the routes
+// GET
+// http://localhost:3000/comments
+// POST
+// http://localhost:3000/comments?username=David&body=I love rabbits!
+// DELETE
+// http://localhost:3000/comments?username=Bob
+// PUT
+// http://localhost:3000/comments?username=David&body=I love dogs!
