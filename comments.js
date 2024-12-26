3fad1a2ec9724907bd837fb791096a7b084d2bb6
modirajendra <modirajendra4@gmail.com>
2024-12-26 00:08:14 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,20 +1,35 @@
 // create web server
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+const bodyParser = require("body-parser");
 
-// create server
-http.createServer(function(req, res) {
-    var pathname = url.parse(req.url).pathname;
-    if (pathname == '/') {
-        display_form(res);
-    } else if (pathname == '/comment') {
-        save_comment(req, res);
-    } else {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end('Not Found');
-    }
-}).listen(1337, () => {
-    console.log('Server running at http://localhost:1337/');
-})
+const DATA_FILE_PATH = path.join(__dirname, "data.json");
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/comments", (req, res) => {
+    fs.readFile(DATA_FILE_PATH, (err, data) => {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        res.json(JSON.parse(data));
+    });
+});
+
+app.post("/api/comments", (req, res) => {
+    fs.readFile(DATA_FILE_PATH, (err, data) => {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        const comments = JSON.parse(data);
+        const newComment = {
+            id: Date.now(),
+        };
+    });
+});
